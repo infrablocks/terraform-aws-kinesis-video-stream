02f93b09e35f819c558e152a78974d4554e23dac
Bamdad Dashtban <bamdad.dashtban@babylonhealth.com>
2021-01-20 22:07:13 +0000

    removing device_name as its unused

--- kinesis_video_stream.tf
+++ kinesis_video_stream.tf
@@ -1,7 +1,6 @@
 resource "aws_kinesis_video_stream" "stream" {
   name                    =  var.stream_name
   data_retention_in_hours =  var.retention
-  device_name             =  var.device_name
   media_type              =  var.media_type 
   kms_key_id              =  aws_kms_key.stream_key.id
   
