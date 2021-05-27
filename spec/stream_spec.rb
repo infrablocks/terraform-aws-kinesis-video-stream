b1f48182594023617a8baabd810fbb3baa4b3c7a
Toby Clemson <tobyclemson@gmail.com>
2021-05-27 16:24:49 +0100

    Use custom expectation.

--- spec/stream_spec.rb
+++ spec/stream_spec.rb
@@ -6,10 +6,13 @@ describe 'Stream' do
   let(:kinesisvideo_client) { Aws::KinesisVideo::Client.new }
 
   let(:stream_arn) { output_for(:harness, "stream_arn") }
+  let(:stream_name) { output_for(:harness, "stream_name") }
 
   subject do
     kinesisvideo_client.describe_stream(stream_arn: stream_arn).stream_info
   end
 
-  it { should exist }
+  it "exists" do
+    expect(subject.stream_name).to(eq(stream_name))
+  end
 end
